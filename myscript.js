195e4af4768828ddacfb5ae1a70571baf4d2191a
Samuel Ardelean <ardeleansamuel@gmail.com>
2023-10-11 20:13:16 +0300

    Update modal open and close

---
+++ myscript.js
@@ -0,0 +1,17 @@
+function imageClickHandler(src) {
+  console.log(src);
+  document.getElementById("image").src = src;
+  document.getElementById("image").style.display = "block";
+  document.querySelector(".preview-container").style.display = "flex";
+}
+// function closeModal() {
+//   document.getElementById("image").src = "";
+//   document.getElementById("image").style.display = "none";
+//   document.querySelector(".preview-container").style.display = "none";
+// }
+
+const closeButton = document.getElementById("closeButton");
+const modal = document.querySelector(".preview-container");
+closeButton.addEventListener("click", () => {
+  modal.style.display = "none";
+});
