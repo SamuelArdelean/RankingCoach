6a56524082b88421fb8546a99af4947f22e5d7b9
Samuel Ardelean <ardeleansamuel@gmail.com>
2023-11-06 19:09:01 +0200

    update c3_cities
    found checkbox soution
    manipulate cities array in javascript

--- c3_cities/cities.js
+++ c3_cities/cities.js
@@ -1,32 +1,56 @@
-let cities = []; // array - we will add city objects in array
+let cities = []; //array - we will add city objects in array
 let cityObj = {}; // object city={name:'',visited:'true or false'}
 document.getElementById("addBttn").addEventListener("click", () => {
   let cityName = document.getElementById("city").value;
   if (cityName.trim() != "") {
     document.querySelector(".citiesListContainer").innerHTML +=
-      "<div class='cityBttnContent'><div class='cityCheckBox'>" +
+      "<div class='cityBttnContent' data-cityName='" +
+      cityName +
+      "'>" +
+      "<div class='cityCheckBox'>" +
       cityName +
       "<input type='checkbox'  class='check'></div>" +
-      "<button type='button' class='deleteBttn' >Delete City</button></div>";
+      "<button type='button' class='deleteBttn'>" +
+      "Delete City" +
+      "</button></div>";
     cityObj = { name: cityName, visited: false };
     cities.push(cityObj); // add cityObj to array
-    console.log(cities);
-    console.log(document.querySelector(".check").checked);
   } else {
     window.alert("Enter a city!");
   }
+  console.log(cities);
 });
 
-document.addEventListener("change", function (event) {
+document.addEventListener("click", function (event) {
   if (event.target.classList.contains("check")) {
-    let x = event.target;
-    event.target.checked = true;
+    console.log(event.target.checked);
+    let checkbox = event.target;
+    let cityElement = checkbox.closest(".cityBttnContent");
+    let cityName = cityElement.getAttribute.trim();
+    for (let i = 0; i <= cities.length - 1; i++) {
+      if ((cities[i].name = cityName)) {
+        cities[i].visited = checkbox.checked;
+        console.log(cities);
+        break;
+      }
+    }
   }
-  console.log(event.target.checked);
 });
 
 document.addEventListener("click", function (event) {
   if (event.target.classList.contains("deleteBttn")) {
+    let deleteBttn = event.target;
+    let cityElement = deleteBttn.closest(".cityBttnContent");
+    let cityName = cityElement.getAttribute("data-cityName");
+
     event.target.parentNode.remove();
+
+    for (let i = 0; i < cities.length; i++) {
+      if (cities[i].name === cityName) {
+        cities.splice(i, 1);
+        console.log(cities);
+        break;
+      }
+    }
   }
 });
