ab8be4b83b4c2024c9da53457c16dfc9f4988c78
Samuel Ardelean <ardeleansamuel@gmail.com>
2023-11-07 17:04:18 +0200

    Update c3_cities (07.11.2023)

--- c3_cities/cities.js
+++ c3_cities/cities.js
@@ -1,20 +1,48 @@
+window.addEventListener("DOMContentLoaded", () => {
+  loadFromLocalStorage();
+  renderList(cities);
+});
+
 let cities = []; //array - we will add city objects in array
 let cityObj = {}; // object city={name:'',visited:'true or false'}
+
+function renderList(cities) {
+  let citiesListContainer = document.querySelector(".citiesListContainer");
+  citiesListContainer.innerHTML = "";
+
+  for (let i = 0; i <= cities.length - 1; i++) {
+    let city = cities[i];
+
+    let cityElement = document.createElement("div");
+    cityElement.classList.add("cityBttnContent");
+    cityElement.setAttribute("data-cityName", city.name);
+
+    cityElement.innerHTML = `<div class=cityCheckBox">${
+      city.name
+    }<input type="checkbox"  class="check"  ${
+      city.visited ? "checked" : ""
+    }></div><button type="button" class="deleteBttn">"Delete City"</button>`;
+
+    citiesListContainer.appendChild(cityElement);
+  }
+}
+
 document.getElementById("addBttn").addEventListener("click", () => {
   let cityName = document.getElementById("city").value;
   if (cityName.trim() != "") {
-    document.querySelector(".citiesListContainer").innerHTML +=
-      "<div class='cityBttnContent' data-cityName='" +
-      cityName +
-      "'>" +
-      "<div class='cityCheckBox'>" +
-      cityName +
-      "<input type='checkbox'  class='check'></div>" +
-      "<button type='button' class='deleteBttn'>" +
-      "Delete City" +
-      "</button></div>";
-    cityObj = { name: cityName, visited: false };
+    // document.querySelector(".citiesListContainer").innerHTML +=
+    //   "<div class='cityBttnContent' data-cityName='" +
+    //   cityName +
+    //   "'>" +
+    //   "<div class='cityCheckBox'>" +
+    //   cityName +
+    //   "<input type='checkbox'  class='check'></div>" +
+    //   "<button type='button' class='deleteBttn'>" +
+    //   "Delete City" +
+    //   "</button></div>";
+    let cityObj = { name: cityName, visited: false };
     cities.push(cityObj); // add cityObj to array
+    renderList(cities);
   } else {
     window.alert("Enter a city!");
   }
@@ -54,3 +82,16 @@ document.addEventListener("click", function (event) {
     }
   }
 });
+
+function saveToLocalStorage() {
+  const data = JSON.stringify(cities);
+  localStorage.setItem("visitedCities", data);
+  console.log(data);
+}
+
+function loadFromLocalStorage() {
+  const data = localStorage.getItem("visitedCities");
+  if (data) {
+    cities = JSON.parse(data);
+  }
+}
